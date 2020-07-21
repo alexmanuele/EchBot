682be5d0fe7ed65b4473e8855c7166dc29f198fb
Alex home <alexmanuele@gmail.com>
2020-07-21 15:42:00 -0300

    Echbot reacts with more echs

--- bot.js
+++ bot.js
@@ -74,6 +74,55 @@ client.on('message', msg => {
 		msg.channel.send(link);
 		
 	}
+	/*
+	if (msg.content.includes("ech")){
+		msg.react("608278702677295114");
+	} */
+	var ech_match = msg.content.match(/ech/g);
+	var num_echs = 0;
+	if (ech_match){
+		num_echs = ech_match.length;
+	}
+	if (num_echs >= 1){
+		msg.react("641623783056080926");
+	}
+	if (num_echs >= 2){
+		msg.react("689165389187645516");
+	}
+	if (num_echs >= 3){
+		msg.react("706929477510365269");
+	}
+	if (num_echs >= 4){
+		msg.react("734844270590033923");
+	}
+	if (num_echs >= 5){
+		msg.react("734844270258946050");
+	}
+	if (num_echs >= 6){
+		msg.react("734844270145568910");
+	}
+	if (num_echs >= 7){
+		msg.react("734844270204420157");
+	}
+	if (num_echs >= 8){
+		msg.react("734844267834507428");
+	}
+	if (num_echs >= 9){
+		msg.react("734844270573256884");
+	}
+	if (num_echs >= 10){
+		msg.react("734844268065062973");
+	}
+	if (num_echs >= 11){
+		msg.react("734844270875246791");
+	}
+	if (num_echs >= 12){
+		msg.react("734844270795554826");
+	}
+	if (num_echs >= 13){
+		msg.react("734844270330118235");
+	}
+
 });
 
 client.login(auth.token);
